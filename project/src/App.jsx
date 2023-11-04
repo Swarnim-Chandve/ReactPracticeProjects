323b1b4928efc63ea0896a5986f9a7aa0ddc75d5
Swarnim Chandve <swarnimchandve23@gmail.com>
2023-11-04 18:21:34 +0530

    added

---
+++ project/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import HeroSection from "./components/HEro";
+import Navigation from "./components/Navigation";
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
